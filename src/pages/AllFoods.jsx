5d2907b0559bd192e139729bbf0ae9a9b052a8e6
PrapooRozario <albart2022@gmail.com>
2025-02-12 02:34:54 +0600

    Added Sorting option in all food page

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,4 +1,13 @@
 import FoodCard from "@/components/FoodCard";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import useAxios from "@/hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
@@ -6,10 +15,11 @@ import { useState } from "react";
 const AllFoods = () => {
   const Axios = useAxios();
   const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
   const { data: foods, isLoading } = useQuery({
-    queryKey: ["foods", search],
+    queryKey: ["foods", search, sort],
     queryFn: async () => {
-      const { data } = await Axios.get(`/foods?search=${search}`);
+      const { data } = await Axios.get(`/foods?search=${search}&sort=${sort}`);
       return data;
     },
   });
@@ -27,13 +37,25 @@ const AllFoods = () => {
           <h1 className="mb-5 text-5xl font-bold text-white">All Foods</h1>
         </div>
       </div>
-      <div className="lg:w-1/2 md:w-2/3 mx-auto mt-6">
+      <div className="lg:w-1/2 md:w-2/3 mx-auto mt-6 flex items-center gap-4">
         <input
           onChange={(e) => setSearch(e.target.value)}
           type="search"
           className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 transition duration-300"
           placeholder="Search for delicious food..."
         />
+        <Select onValueChange={(e)=>setSort(e)}>
+          <SelectTrigger className="w-[180px] h-12">
+            <SelectValue placeholder="Sort by price" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Sort Order</SelectLabel>
+              <SelectItem value="desc">High to Low</SelectItem>
+              <SelectItem value="asc">Low to High</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
 
       {isLoading ? (
